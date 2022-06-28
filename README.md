938a77f903efa6cce2349c20dab94896fd001b03
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2022-06-28 13:42:40 +0300

    sfml linked

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # deriv
+
+install sfml:
+
+
+clion on linux tutorial:
+https://medium.com/@Ancyr/install-sfml-2-5-1-on-ubuntu-18-04-and-clion-9e0dfe86e87f
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "transformation/InfixToPostfix.h"
+#include "transformations/InfixToPostfix.h"
 #include "utils/Node.h"
-#include "transformation/PostfixToBinaryTree.h"
-
+#include "transformations/PostfixToBinaryTree.h"
+#include <SFML/Graphics.hpp>
 using namespace std;
 int main() {
     string infix, postfix;
@@ -10,5 +10,24 @@ int main() {
     postfix=InfixToPostfix::transform(infix);
     Node *top= PostfixToBinaryTree::transform(postfix);
     cout<<top->value;
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
